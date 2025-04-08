4c1c38a8f2c837bf057ecea250c48a3173f3a9b8
cardrank <cardrank@protonmail.com>
2025-04-08 11:42:57 +0700

    pull test

--- dist_test.go
+++ dist_test.go
@@ -1,7 +1,9 @@
 package tripled
 
 import (
+	"maps"
 	"math/rand"
+	"slices"
 	"strconv"
 	"testing"
 	"time"
@@ -33,6 +35,34 @@ func TestDist(t *testing.T) {
 				payout, wagred,
 				100.0*float64(payout)/float64(wagred),
 			)
+		})
+	}
+}
+
+func TestDistPulls(t *testing.T) {
+	const bet = 100
+	const starting = 10000
+	for n := range 9 {
+		lines := n + 1
+		t.Run(strconv.Itoa(lines), func(t *testing.T) {
+			r := rand.New(rand.NewSource(time.Now().UnixNano()))
+			running, pulls := starting, 0
+			m := make(map[int]int)
+			for running > 0 && running >= bet*lines {
+				res, err := DefaultDist.Spin(r, lines)
+				if err != nil {
+					t.Fatalf("expected no error, got: %v", err)
+				}
+				running += -bet*lines + bet*res.Payout
+				m[res.Payout]++
+				pulls++
+			}
+			t.Logf("pulls: %d", pulls)
+			keys := slices.Sorted(maps.Keys(m))
+			slices.Reverse(keys)
+			for _, k := range keys {
+				t.Logf("% 5d: %d", k, m[k])
+			}
 		})
 	}
 }
